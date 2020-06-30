932c839efeff6743be520e6264a64b255850816c
Lavanya Ganesh <lavanya.ganesh78@gmail.com>
2020-06-29 20:17:14 -0400

    chloropleth base code push

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("map", {
   center: [34.0522, -118.2437],
-  zoom: 8
+  zoom: 10
 });
 
 // Adding tile layer
@@ -15,72 +15,71 @@ var tile = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
 }).addTo(myMap)
 
 // // Load in geojson data
- var geoData = "static/data/gz_2010_geojson.json";
+ var geoData = "static/data/geojson_withNAs.json";
 
+ console.log(geoData);
  var geojson;
 
-// // // Grab data with d3
-  d3.json(geoData, function(data) {
 
-// //   // Create a new choropleth layer
-    // geojson = L.choropleth(data, {
-        console.log(data);
-
-    // });
-});
-
-// //     // Define what  property in the features to use
-//          valueProperty: "GEO_ID",
-
-// // //     // Set color scale
-//      scale: ["#ffffb2", "#b10026"],
-
-// // //     // Number of breaks in step range
-//      steps: 10,
-
-// //     // q for quartile, e for equidistant, k for k-means
-//     mode: "q",
-//     style: {
-//       // Border color
-//       color: "#fff",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     }
-//   }).addTo(myMap);
-// }
-// //     // Binding a pop-up to each layer
-// //     onEachFeature: function(feature, layer) {
-// //       layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-// //         "$" + feature.properties.MHI2016);
-// //     }
-// //   }).addTo(myMap);
-
-// //   // Set up the legend
-// //   var legend = L.control({ position: "bottomright" });
-// //   legend.onAdd = function() {
-// //     var div = L.DomUtil.create("div", "info legend");
-// //     var limits = geojson.options.limits;
-// //     var colors = geojson.options.colors;
-// //     var labels = [];
-
-// //     // Add min & max
-// //     var legendInfo = "<h1>Median Income</h1>" +
-// //       "<div class=\"labels\">" +
-// //         "<div class=\"min\">" + limits[0] + "</div>" +
-// //         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-// //       "</div>";
-
-// //     div.innerHTML = legendInfo;
-
-// //     limits.forEach(function(limit, index) {
-// //       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-// //     });
-
-// //     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-// //     return div;
-// //   };
-
-// //   // Adding legend to the map
-// //   legend.addTo(myMap);
-
-
+ // Grab data with d3 use the then function  since we have version 5
+ d3.json(geoData).then (function(data) {
+  console.log(data)
+   // Create a new choropleth layer
+   geojson = L.choropleth(data, {
+ 
+    
+     // Define what  property in the features to use
+     valueProperty: "GEO_ID",
+ 
+     // Set color scale
+     scale: ["#ffffb2", "#b10026"],
+ 
+     // Number of breaks in step range
+     steps: 10,
+ 
+     // q for quartile, e for equidistant, k for k-means
+     mode: "q",
+     style: {
+       // Border color
+       color: "#fff",
+       weight: 1,
+       fillOpacity: 0.8
+     },
+ 
+     // Binding a pop-up to each layer
+     onEachFeature: function(feature, layer) {
+       layer.bindPopup("CountyName: " + feature.properties.NAME + "<br>Avg  Air Quality index<br>" +
+          + feature.properties.aqi_avg);
+     }
+   }).addTo(myMap);
+ 
+   // Set up the legend
+  //  var legend = L.control({ position: "bottomright" });
+  //  legend.onAdd = function() {
+  //    var div = L.DomUtil.create("div", "info legend");
+  //    var limits = geojson.options.limits;
+  //    var colors = geojson.options.colors;
+  //    var labels = [];
+ 
+  //    // Add min & max
+  //    var legendInfo = "<h1>Median Income</h1>" +
+  //      "<div class=\"labels\">" +
+  //        "<div class=\"min\">" + limits[0] + "</div>" +
+  //        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //      "</div>";
+ 
+  //    div.innerHTML = legendInfo;
+ 
+  //    limits.forEach(function(limit, index) {
+  //      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //    });
+ 
+  //    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //    return div;
+  //  };
+ 
+  //  // Adding legend to the map
+  //  legend.addTo(myMap);
+ 
+ });
+ 
